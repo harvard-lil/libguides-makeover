b6a7a56852c1eac2c938b7cc90a414b283d59a9c
Annie Cain <anniejocain@gmail.com>
2015-06-25 14:51:39 -0400

    new libguides styles and js

--- web/js/landing.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
- $.getJSON("http://hlsl7.law.harvard.edu/dev/annie/research/guide103327.json", function(sections) {
-    var source = $("#guide-template").html();
-    var template = Handlebars.compile(source);
-    $('#guide').append(template(sections));
-  });});
