c3f465ec82655de50d8fb105ae6f16da6e0080a1
Annie Cain <anniejocain@gmail.com>
2015-06-25 14:59:26 -0400

    settings file

---
+++ settings.example.js
@@ -0,0 +1,5 @@
+var config = {
+  apiUrl: 'http://lgapi.libapps.com/1.1/accounts/?site_id=YOURSITEID&key=YOURKEY&expand=profile', 
+  imageBase: 'http://s3.amazonaws.com/libapps/accounts/', 
+  imageBackground: 'http://s3.amazonaws.com/libapps/apps/common/images/profile.jpg'
+}
