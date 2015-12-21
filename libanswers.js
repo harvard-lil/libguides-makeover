6f57e97e985301bb1bd7f5f3a631298b58da6980
Annie Cain <anniejocain@gmail.com>
2015-12-21 10:02:25 -0500

    answers search results styling

--- libanswers.js
+++ libanswers.js
@@ -2,4 +2,5 @@ $(document).ready(function() {
 
     // Anything?
     $(".s-la-navbrowse li:nth-child(2) a").text('View All');
+    $('#s-la-search-askbutton').insertAfter('#s-la-public-search-header');
 });
